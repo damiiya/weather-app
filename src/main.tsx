1bd8bfa2bc7e60696137c52299ebcfe81f31096f
damiiya <ddam2lee@gmail.com>
2026-01-11 17:57:16 +0900

    fix: 경로 오타 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '@/app/global.css';
+import '@/app/styles/global.css';
 import QueryProvider from '@/app/providers/QueryProvider.tsx';
 
 import App from './App.tsx';
