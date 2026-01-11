a80f185d3bbe15307866143e344016b21b516607
damiiya <ddam2lee@gmail.com>
2026-01-11 14:06:11 +0900

    fix: global.css 파일 위치 이동 & 라우트 404 처리 추가

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -5,16 +5,20 @@ import { FavoritesPage } from '@/pages/favorites';
 import { HomePage } from '@/pages/home';
 import { NotFoundPage } from '@/pages/not-found';
 
+const errorElement = <NotFoundPage />;
+
 export const router = createBrowserRouter([
-  { path: '/', element: <HomePage />, errorElement: <NotFoundPage /> },
+  { path: '/', element: <HomePage />, errorElement },
   {
     path: '/favorites',
     element: <FavoritesPage />,
-    errorElement: <NotFoundPage />,
+    errorElement,
   },
   {
     path: '/favorites/:id',
     element: <FavoriteDetailPage />,
-    errorElement: <NotFoundPage />,
+    errorElement,
   },
+
+  { path: '*', element: <NotFoundPage /> },
 ]);
